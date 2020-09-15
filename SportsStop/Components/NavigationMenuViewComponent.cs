23e252b4a9afcb472afd8b1d891a6e5432ab5492
Alejandro Mayorga <alejandro.mayorga11@outlook.com>
2020-09-15 15:55:20 -0700

    Add project files.

---
+++ SportsStop/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SportsStop.Models;
+
+namespace SportsStop.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IStoreRepository repository;
+
+        public NavigationMenuViewComponent(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(repository.Products
+            .Select(x => x.Category)
+            .Distinct()
+            .OrderBy(x => x));
+        }
+    }
+}
---
+++ SportsStop/Models/SeedData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStop.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            StoreDbContext context = app.ApplicationServices
+            .CreateScope().ServiceProvider.GetRequiredService<StoreDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                //context.Database.Migrate();
+            }
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                new Product
+                {
+                    Name = "Test",
+                    Description = "object 1",
+                    Category = "testing",
+                    Price = 100
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
